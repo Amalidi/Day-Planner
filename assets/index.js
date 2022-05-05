082411e6d55ad6288d93e6673bb6709f16c38faf
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-05-05 15:04:53 +0100

    added each hour into an aarray

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,54 @@ console.log("hello")
 
 // render the time 
 const renderDate = () => {};
+
+// render each time block
+const renderTimeBlocks = () => {};
+
+const workingHours = [
+    {
+        label: "9AM" , 
+        key : 9
+    },
+
+    {
+        label: "10AM" , 
+        key : 10
+    },
+
+    {
+        label: "11AM" , 
+        key : 11
+    },
+
+    {
+        label: "12PM" , 
+        key : 12
+    },
+
+    {
+        label: "1PM" , 
+        key : 1
+    },
+
+    {
+        label: "2PM" , 
+        key : 2
+    },
+
+    {
+        label: "3PM" , 
+        key : 3
+    },
+
+    {
+        label: "4PM" , 
+        key : 4
+    },
+
+    {
+        label: "5PM" , 
+        key : 5
+    },
+
+]
