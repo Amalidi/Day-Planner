d573538db44e0b1f3ce7bdb0c5b4c0ae2fc75d61
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-05-08 17:10:05 +0100

    added the timer and timer blocks

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,6 @@
 console.log("hello")
 
-// render each time block
-const renderTimeBlocks = () => {};
+
 
 const workingHours = [
     {
@@ -51,8 +50,71 @@ const workingHours = [
 
 ]
 
+const getEventForTimeBlock = () => {
+
+}
+
+
+const renderTimeBlocks = () => {
+    const timeBlocks = $('#time-blocks')
+
+    const renderTimeBlock = (workingHours) => {
+        console.log(workingHours);
+        // create time block
+        const timeBlock = `<div class="row p-2">
+        <div class="col-md-1 col-sm-12 text-center my-1 d-flex flex-column justify-content-center">${workingHours.label} </div>
+        <textarea class="col-md-9 col-sm-12" rows="3">${getEventForTimeBlock(workingHours.key)}</textarea>
+        <div  class="col-md-2 col-sm-12 text-center my-1 d-flex flex-column justify-content-center">
+        <button type="button" data-hour=${workingHours.key} class="btn btn-success">Save</button>
+        </div>`;
+        // append time block
+        timeBlocks.append(timeBlock)
+        
+      };
+      workingHours.forEach(renderTimeBlock);
+
+    };
+
+const onReady = () => {
+    console.log("ready")
+    renderTimeBlocks();
+}
+
+$(document).ready(onReady);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // display current date and time from moment.js
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+// const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+const renderDate = () => {
+    const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").append(currentDate);
+  };
+  
 
 
 // to click the button the textarea will need to be targetted
@@ -61,21 +123,80 @@ $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 // on click store in local storage 
 // add a data attribute on the button (eg. data-key = 17)
 
+//function to handle click on save buttons
+// const saveButtonClick = (event) => {
+//     //gets the data-key for the button clicked
+//     const targetKey = $(event.target).attr("data-key");
+//     //gets the value of the corresponding textarea (textarea with same data-key)
+//     const taskValue = $(`textarea[data-key=${targetKey}]`).val();
+//     //sets the data-key and its value in local storage (for data persistence)
+//     writeToLS(targetKey, taskValue);
+//   };
+
 const namebtn = $("#All-btn");
 
-const handleAllButtonClick = () => {
-    console.log("view btn click");
-};
+// const handleAllButtonClick = () => {
+//     console.log("view btn click");
+// };
 
-//function to handle click on save buttons
+// console.log(namebtn)
+
+// $(".saveBtn").on("click", function () {
+//     //get nearby values.
+//     console.log(this);
+//     var text = $(this).siblings(".description").val();
+//     var time = $(this).parent().attr("id");
+
+//     //set items in local storage.
+//     localStorage.setItem(time, text);
+// })
 
 
-namebtn.click(handleAllButtonClick);
 
-console.log(namebtn)
+//    //function to render the time blocks on the page
+//    const renderTimeBlock = () => {
+//     //fetches the current hour with moment.js
+//     const currentTime = moment().hour();
+  
+//     //each time block
+//     const renderEachBlock = (each) => {
+//         $("#container").append(
+//           $("<div>")
+//             .addClass("row time-block")
+//             .addClass("row")
+//             .attr("id", `timer-box-${each.key}`)
+//             .attr("data-key", `${each.key}`)
+//             .append(
+//               $("<span>")
+//                 .addClass("col-md-1 hour")
+//                 .attr("id", `timer-hour-${each.key}`)
+//                 .attr("data-key", `${each.key}`)
+//                 .html(`${each.label}`),
+//               $("<textarea>")
+//                 .addClass(`col-md-10 description}`)
+//                 .attr("data-key", `${each.key}`),
+//               $("<button>")
+//                 .addClass("btn saveBtn col-md-1")
+//                 .attr("id", `All-btn-${each.key}`)
+//                 .attr("type", "button")
+//                 .attr("data-key", `${each.key}`)
+//                 .attr("data-purpose", "save")
+//                 .html('<i class="fas fa-save"></i>'),
+//             )
+//         );
+//     };
+  
+//     //stored in the working hours array
+//     workingHours.forEach(renderEachBlock);
+//   };
 
 
-
-
-
-
+//Main function triggered on load
+const renderPlanner = () => {
+    // render date in header
+    renderDate();
+    renderTimeBlock();
+  };
+  
+  //instruction on load of page
+  $(window).on("load", renderPlanner);
