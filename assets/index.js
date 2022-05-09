de1822d3ef7fdc20a2fbc5bd9b5e3a7fe9fcedb7
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-05-09 17:09:08 +0100

    added local storage

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,6 @@ console.log("hello")
 
 const timeBlocks = $("#time-blocks")
 
-
-
 const workingHours = [
     {
         label: "9AM" , 
@@ -50,19 +48,22 @@ const workingHours = [
         key : 17
     },
 
+    {
+        label: "6PM" , 
+        key : 18
+    },
+
+    {
+        label: "7PM" , 
+        key : 19
+    },
 ]
 
 // display current date and time from moment.js
-// const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 const renderDate = () => {
     const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
     $("#currentDay").append(currentDate);
   };
-
-const getEventForTimeBlock = (workingHours) => {
-    // const planner = readFromLocalStorage("planner",{})
-    // return planner [workindDay]
-}
 
 const getClassName = (workingHours) => {
    const currentHour = moment().hour();
@@ -71,11 +72,11 @@ const getClassName = (workingHours) => {
         return "present";
     }
     if(workingHours >currentHour){
-        // when workingHours is future
+    // when workingHours is future
     return "future"
     }
+    // when workingHours 
     return "past"
-    // when workingHours is future
 };
 
 
@@ -112,9 +113,11 @@ const saveToLS = (event) =>{
         const value = $(`textarea[data-textarea-key = '${key}']`).val().trim();
         console.log(value)
 
-        // const planner = readFromLocalStorage("planner",{})
-        // planner[key] = value
-        // writeToLocalStorage
+        const planner = readFromLocalStorage("planner", {});
+
+        planner[key] = value;
+
+        writeToLocalStorage("planner", planner);
     }
 };
 
@@ -139,126 +142,11 @@ const readFromLocalStorage = (key, defaultValue) => {
     // set stringified value to LS for key name
     localStorage.setItem(key, stringifiedValue);
   };
-
+  
+  const getEventForTimeBlock = (workingDay) => {
+    const planner = readFromLocalStorage("planner",{});
+    return planner[workingDay] || "";
+  };
 
 timeBlocks.click(saveToLS)
 $(document).ready(onReady);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// display current date and time from moment.js
-// const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-// const renderDate = () => {
-//     const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-//     $("#currentDay").append(currentDate);
-//   };
-  
-
-
-// to click the button the textarea will need to be targetted
-// then save it in local storage
-// add an event Listener to the container div
-// on click store in local storage 
-// add a data attribute on the button (eg. data-key = 17)
-
-//function to handle click on save buttons
-// const saveButtonClick = (event) => {
-//     //gets the data-key for the button clicked
-//     const targetKey = $(event.target).attr("data-key");
-//     //gets the value of the corresponding textarea (textarea with same data-key)
-//     const taskValue = $(`textarea[data-key=${targetKey}]`).val();
-//     //sets the data-key and its value in local storage (for data persistence)
-//     writeToLS(targetKey, taskValue);
-//   };
-
-// const namebtn = $("#All-btn");
-
-// const handleAllButtonClick = () => {
-//     console.log("view btn click");
-// };
-
-// console.log(namebtn)
-
-// $(".saveBtn").on("click", function () {
-//     //get nearby values.
-//     console.log(this);
-//     var text = $(this).siblings(".description").val();
-//     var time = $(this).parent().attr("id");
-
-//     //set items in local storage.
-//     localStorage.setItem(time, text);
-// })
-
-
-
-//    //function to render the time blocks on the page
-//    const renderTimeBlock = () => {
-//     //fetches the current hour with moment.js
-//     const currentTime = moment().hour();
-  
-//     //each time block
-//     const renderEachBlock = (each) => {
-//         $("#container").append(
-//           $("<div>")
-//             .addClass("row time-block")
-//             .addClass("row")
-//             .attr("id", `timer-box-${each.key}`)
-//             .attr("data-key", `${each.key}`)
-//             .append(
-//               $("<span>")
-//                 .addClass("col-md-1 hour")
-//                 .attr("id", `timer-hour-${each.key}`)
-//                 .attr("data-key", `${each.key}`)
-//                 .html(`${each.label}`),
-//               $("<textarea>")
-//                 .addClass(`col-md-10 description}`)
-//                 .attr("data-key", `${each.key}`),
-//               $("<button>")
-//                 .addClass("btn saveBtn col-md-1")
-//                 .attr("id", `All-btn-${each.key}`)
-//                 .attr("type", "button")
-//                 .attr("data-key", `${each.key}`)
-//                 .attr("data-purpose", "save")
-//                 .html('<i class="fas fa-save"></i>'),
-//             )
-//         );
-//     };
-  
-//     //stored in the working hours array
-//     workingHours.forEach(renderEachBlock);
-//   };
-
-
-//Main function triggered on load
-const renderPlanner = () => {
-    // render date in header
-    renderDate();
-    // renderTimeBlock();
-  };
-  
-  //instruction on load of page
-  $(window).on("load", renderPlanner);
